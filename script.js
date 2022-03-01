b7f22c2fd79c371e71fed639066272384fc7a2a3
PannDev <86787046+PannDev@users.noreply.github.com>
2022-03-02 02:23:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// Hamburger
+
+let menu = document.querySelector('.burger_menu');
+console.log(menu);
+menu.addEventListener('click', setMenu);
+
+
+function setMenu() {
+    let Setbody = document.querySelector('body');
+    Setbody.classList.toggle('menu_active');
+}
+
+let nav = document.querySelectorAll('.nav_center a');
+console.log(nav);
+
+for (const key in nav) {
+    if (Object.hasOwnProperty.call(nav, key)) {
+        const addMenu = nav[key];
+        console.log(addMenu);
+        addMenu.addEventListener('click', Setnav);
+
+    }
+}
+
+function Setnav() {
+    let Setbody = document.querySelector('body');
+
+    Setbody.classList.remove('menu_active');
+}
